b14035e33361f2e773ce398d77504c947e1332ce
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-02-01 14:45:23 +0100

    created empty project

---
+++ android/src/info/piwai/fosdemsms/FosdemSmsActivity.java
@@ -0,0 +1,13 @@
+package info.piwai.fosdemsms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FosdemSmsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
